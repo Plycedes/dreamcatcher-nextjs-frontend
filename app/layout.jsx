78d0b3488805790f042821bd022fdc41a338bb76
Plycedes <abhaypratapyadav340@gmail.com>
2023-09-23 11:02:04 +0530

    changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,9 @@ import "@styles/globals.css";
 import { MoralisProvider } from "react-moralis";
 import Header from "@components/Header";
 
+const APP_ID = process.env.NEXT_PUBLIC_APP_ID;
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
+
 export const metadata = {
     title: "One Piece",
     description: "Decentralized Lottery",
@@ -13,7 +16,7 @@ const RootLayout = ({ children }) => {
     return (
         <html lang="en">
             <body>
-                <MoralisProvider initializeOnMount={false}>
+                <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
                     <div className="main">
                         <main className="app">
                             <Header />
